8bd32515f4c5f88ea9a1fa10fd313c6adcdca4cb
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-08-14 17:09:45 -0600

    initial commit

---
+++ src/links.js
@@ -0,0 +1,48 @@
+export const mainContent = {
+  imgSrc: `https://jaydanurwin.com/assets/images/components/profile-image-2021.jpg`,
+  name: `Jaydan Urwin`,
+  tagline: `Designer & Developer, creates videos on YouTube`
+};
+
+export const socialLinks = [
+  {
+    title: 'Instagram',
+    icon: 'mdi:instagram',
+    url: 'https://www.instagram.com/linkedupapp/',
+  },
+  {
+    title: 'Twitter',
+    icon: 'mdi:twitter  ',
+    url: 'https://twitter.com/linkedupapp',
+  },
+  {
+    title: 'Facebook',
+    icon: 'ic:baseline-facebook',
+    url: 'https://www.facebook.com/linkedupapp/',
+  },
+  {
+    title: 'GitHub',
+    icon: 'mdi:github',
+    url: 'https://github.com',
+  }
+]
+
+export const links = [
+  {
+    text: 'Home',
+    url: '/',
+    featured: true,
+  },
+  {
+    text: 'About',
+    url: '/about',
+  },
+  {
+    text: 'Uses',
+    url: '/uses',
+  },
+  {
+    text: 'See my latest blog post!',
+    url: '/blog'
+  }
+]
